08586bec48b423e87083570a59060050b2e10905
Tempsyche <ruinature7@gmail.com>
2026-01-08 03:28:18 +0800

    内容填充

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -21,22 +21,40 @@ export const projects: ProjectData[] = [
         category: "UX Design",
         color: "#3e1c1c",
         imagePlaceholder: "CP",
-        src: "/images/1.PNG",
+        src: "/images/1.png",
         description: "An analysis of the user ecosystem and user stickiness based on cluster analysis and Markov chain modeling.",
         // 详情图
         detailImages: [
             "/images/projects/1/1-1.png",
-            "/images/projects/1/1-2.png"
+            "/images/projects/1/1-2.png",
+            "/images/projects/1/1-3.png",
+            "/images/projects/1/1-4.png",
+            "/images/projects/1/1-5.png"
         ]
     },
     {
         id: 2,
         title: "AI Fundamentals Pre",
         year: "2025",
-        category: "AI",
+        category: "AI Theories",
         color: "#4a2b22",
         imagePlaceholder: "HO",
-        src: "/images/2.PNG"
+        src: "/images/2.PNG",
+        description: "About Schrödinger Bridge.",
+        // 详情图
+        detailImages: [
+            "/images/projects/2/2-1.png",
+            "/images/projects/2/2-2.png",
+            "/images/projects/2/2-3.png",
+            "/images/projects/2/2-4.png",
+            "/images/projects/2/2-5.png",
+            "/images/projects/2/2-6.png",
+            "/images/projects/2/2-7.png",
+            "/images/projects/2/2-8.png",
+            "/images/projects/2/2-9.png",
+            "/images/projects/2/2-10.png",
+            "/images/projects/2/2-11.png"
+        ]
     },
     {
         id: 3,
